80a6e5a49550ec4164ed582547efa138df7152c7
Evgeniy Kunegin <kunegin@gmail.com>
2024-01-07 19:11:53 +0400

    Settings of Jenkinsfile and script.groovy
    Steps in JenkinsFile link to parts of scritp.groovy

--- script.groovy
+++ script.groovy
@@ -1,5 +1,14 @@
 def buildApp() {
     echo 'building the application'
+    sh 'mvn package'
+}
+
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')])
+        sh "docker build -t evgenk25/demo-app:jma-2.0 ."
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh "docker push evgenk25/demo-app:jma-2.0"
 }
 
 def testApp() {
